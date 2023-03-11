4935e8d3de61625081b07dbac89255113825cfda
SASHiBHUMARAJU2 <sasi.654forever@gmail.com>
2023-03-11 22:24:06 +0530

    'updated'

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import HorizontalScroll from 'react-scroll-horizontal'
 import './Skills.css';
 import bulb from '../assets/skill.json';
@@ -34,29 +34,40 @@ import right from '../assets/right.svg';
     // }
   };
 
+  function leftScroll () {
+     var slider = document.getElementById('Slider');
+     slider.scrollLeft = slider.scrollLeft - slider.offsetWidth;
+  }
+
+  function rightScroll () {
+    var slider = document.getElementById('Slider');
+    slider.scrollLeft = slider.scrollLeft +  slider.offsetWidth;
+  }
+
+  
+
   const child   = { width: `30em`, height: `100px`}
-    
+  
+
+  
     return(
         <div className='Skills'>
-        
-
             <div className='SkillsTitle'> About Me and My Skills  </div>
             <div className='SkillsAboutMe'> As a <b> passionate </b> and driven computer science and engineering student with experience in various <b>programming languages and technologies.</b> My passion for <b> mobile development</b> and interface design started with developing an Android app using <b> Flutter </b> and <b>Android Studio.</b> I have honed my problem-solving skills in <b> data structures and algorithms</b> through online resources. I have also <b> developed</b> web projects using <b>React JS </b>and <b>Java Spring Boot </b>with dynamic and intuitive interfaces. I am a versatile and motivated developer committed to producing <b> efficient solutions </b> to complex problems.</div>
 
             <div className='SkillsNames' >
-              <img className='arrow' src={left}></img>
-              <HorizontalScroll>
-                  {cardData.map((v)=>{
-                    if(v.icon === "-") return <div style={{width :'20px'}}></div>
-                    else return <div  className='SkillsNamesCard' > 
+              <img className='arrow' src={left} onClick={leftScroll}></img>
+               <div id="Slider" className='SkillsCardList'>
+                  {cardData.map((v,i)=>{
+                    if(v.icon === "-") return <div key={i} className='EmptyBox'>  </div>
+                    else return <div key={i}  className='SkillsNamesCard' > 
                                 <img className='SkillsNamesCardImage' src={v.icon}/>  
                                 <div className='SkillsNamesCardText'> {v.name}</div>
                               </div>
                     })}  
-              </HorizontalScroll>
-              <img className='arrow' src={right}></img>         
+                </div>
+              <img className='arrow' src={right} onClick={rightScroll}></img>         
            </div>
-
       </div>
     )
  }
