7cda47bbf5cdcfa3e04706e5dedf7fc1cc6d4c2d
SASHiBHUMARAJU2 <sasi.654forever@gmail.com>
2023-03-21 18:16:32 +0530

    'updated'

--- src/components/HamburgerPage.js
+++ src/components/HamburgerPage.js
@@ -57,18 +57,24 @@ function HamburgerPage () {
 const [isOpen, setOpen] = useState(false)
 
 
+const [slideOut, setslideOut] = useState("");
+
+
+
     return <> <div className={ isScrollHam? 'HamburgerLight' : 'HamburgerDark' }><Hamburger toggled={toggleSlide}  duration={0.1} direction='right' size={24} 
         onToggle={ active => {
             if( active ) {
-               console.log("opened");
-               setToggleSlide(true);
+               console.log("opened"); 
+               setToggleSlide(true); 
+               
             } else {
                console.log('closed')
                setToggleSlide(false);
+               setslideOut("slide-out"); 
             }
         }}
     ></Hamburger></div> 
-    <div   className={ isScrollHam?  toggleSlide? 'slide-in MenuLight' : 'slide-out MenuLight' :  toggleSlide? 'slide-in MenuDark' : 'slide-out MenuDark'   }>
+    <div   className={ isScrollHam?  toggleSlide? 'slide-in MenuLight' : `${slideOut} MenuLight`  :  toggleSlide? 'slide-in MenuDark' : `${slideOut} MenuDark`  }>
        {links}
     </div>
     </>
